68d104cd24dadf7b9e62df24a5fe837da45bd4d4
Nani <nanipinninti123@gmail.com>
2024-09-07 18:35:28 +0530

    Protected route done

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css'
+
+const ProtectedRoute = (props)=>{
+    const {element} = props
+    const login_token = Cookies.get("login_token")
+    if (login_token===undefined) {
+        return (<Navigate to = {'\login'}/>)
+    }
+    return (element)
+}
+export default ProtectedRoute
